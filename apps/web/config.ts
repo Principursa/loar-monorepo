303e790f076909a70768b93887d976b4896fd07c
Pattisier <Principursa@protonmail.com>
2025-08-16 17:54:43 -0400

    added wagmi based abi generation and hooks

--- apps/web/config.ts
+++ apps/web/config.ts
@@ -1,6 +1,11 @@
 import { createConfig, http } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
 export const config = createConfig({
   chains: [mainnet, sepolia],
   transports: {
