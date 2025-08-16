9402a58a2af06f074b93a71f694ca7e0f3c72bb0
Pattisier <Principursa@protonmail.com>
2025-08-15 21:20:04 -0400

    readded contracts

---
+++ apps/contracts/script/Counter.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract CounterScript is Script {
+    Counter public counter;
+
+    function setUp() public {}
+
+    function getChainId() public view returns (uint256) {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+        return chainId;
+    }
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerAddress = vm.addr(deployerPrivateKey);
+        console.log("Deployer address: ", deployerAddress);
+        console.log("Deployer balance: ", deployerAddress.balance);
+        console.log("BlockNumber: ", block.number);
+        console.log("ChainId: ", getChainId());
+        console.log("Deploying");
+        vm.startBroadcast(deployerPrivateKey);
+
+        counter = new Counter();
+
+        vm.stopBroadcast();
+    }
+}
