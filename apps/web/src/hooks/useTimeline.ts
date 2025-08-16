61d528633b2eb15d5b62d0ce784e293b0174b040
Pattisier <Principursa@protonmail.com>
2025-08-16 19:47:32 -0400

    added useGetMedia hook

--- apps/web/src/hooks/useTimeline.ts
+++ apps/web/src/hooks/useTimeline.ts
@@ -2,6 +2,7 @@ import { useReadContract, useWriteContract } from 'wagmi'
 import { timelineAbi } from '@/generated'
 import { useChainId } from 'wagmi'
 import { TIMELINE_ADDRESSES, SupportedChainId } from '@/configs/addresses-test'
+import { TimelineEventNode } from '@/components/flow/TimelineNodes'
 
 
 //----------READ FUNCTIONS---------
@@ -23,7 +24,15 @@ export function useGetLeaves() {
 
 }
 
-export function useGetMedia() {
+export function useGetMedia(id: number) {
+  const chainId = useChainId()
+
+  return useReadContract({
+    abi: timelineAbi,
+    address: TIMELINE_ADDRESSES[chainId as SupportedChainId],
+    functionName: "getMedia",
+    args: [id]
+  })
 
 }
 export function useGetCanonChain() {
