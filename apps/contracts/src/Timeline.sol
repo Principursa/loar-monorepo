5cfedca4ef56f2bdcad4f70b0f96be28f5d7bebe
Ast <varistyshark@gmail.com>
2025-09-11 12:52:00 -0400

    moved deps to soldeer

--- apps/contracts/src/Timeline.sol
+++ apps/contracts/src/Timeline.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Ownable} from "@openzeppelin/access/ownable.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
 
 contract Timeline is Ownable {
     struct VideoNode {
