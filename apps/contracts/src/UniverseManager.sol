0cff58aa6f5f9728caad663348ed6ed43f2207df
Ast <varistyshark@gmail.com>
2025-10-09 16:15:54 -0400

    began working on new contracts

---
+++ apps/contracts/src/UniverseManager.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+import {Script, console} from "forge-std/Script.sol";
+import {GovernanceERC20} from "./GovernanceERC20.sol";
+import {UniverseGovernor} from "./UniverseGovernor.sol";
+import {Universe} from "./Universe.sol";
+
+contract UniverseManager {
+
+}
