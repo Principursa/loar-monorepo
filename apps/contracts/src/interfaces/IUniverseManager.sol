faea576e863ff21ab97217c522555028b838157a
Ast <varistyshark@gmail.com>
2025-10-11 20:21:27 -0400

    interfaces

---
+++ apps/contracts/src/interfaces/IUniverseManager.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.30;
+
+interface IUniverseManager {
+
+  event UniverseCreated(address creator, address universe,address token, address governance);
+
+
+}
