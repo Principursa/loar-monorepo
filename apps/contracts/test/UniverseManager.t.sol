4989c585a5cd556689266d345bff5bea01ed1fd5
Ast <varistyshark@gmail.com>
2025-11-04 15:35:23 -0500

    got hook deployment working

--- apps/contracts/test/UniverseManager.t.sol
+++ apps/contracts/test/UniverseManager.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {UniverseManager} from "../src/UniverseManager.sol";
 import {NodeCreationOptions, NodeVisibilityOptions} from "../src/libraries/NodeOptions.sol";
 
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
@@ -41,16 +42,46 @@ contract UniverseManagerTest is HookTest {
         console.log(address(universeManager), "UniverseManager Address");
         WETH = WETH9(payable(address(123)));
         console.log(address(WETH), "WETH Address");
-        loarHook = new LoarHookStaticFee(
-            address(poolManager),
-            address(universeManager),
-            address(WETH)
+        //loarHook = new LoarHookStaticFee(
+        //    address(poolManager),
+        //    address(universeManager),
+        //   address(WETH)
+        //);
+
+        loarHook = LoarHookStaticFee(
+            address(
+                uint160(
+                    Hooks.BEFORE_INITIALIZE_FLAG |
+                        Hooks.BEFORE_ADD_LIQUIDITY_FLAG |
+                        Hooks.BEFORE_SWAP_FLAG |
+                        Hooks.AFTER_SWAP_FLAG |
+                        Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG |
+                        Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
+                )
+            )
+        );
+        deployCodeTo(
+            "src/hooks/LoarHookStaticFee.sol:LoarHookStaticFee",
+            abi.encode(
+                address(poolManager),
+                address(universeManager),
+                address(WETH)
+            ),
+            address(loarHook)
         );
         console.log(address(loarHook), "HookStaticFee Address");
         lrWETH = WETH;
     }
 
     function test_CreateUniverse() public {
+        assertNotEq(address(0), universeAddress);
+    }
+
+    function test_deployUniverseToken() public {}
+
+    function test_claimFee() public {}
+
+    function createUniverse() public returns (address) {
         (uint id, address universeAddress) = universeManager.createUniverse(
             "One Piece",
             "random.png",
@@ -59,10 +90,6 @@ contract UniverseManagerTest is HookTest {
             NodeVisibilityOptions.PUBLIC,
             address(this)
         );
-        assertNotEq(address(0), universeAddress);
+        return universeAddress;
     }
-
-    function test_deployUniverseToken() public {}
-
-    function test_claimFee() public {}
 }
