29d0be7092f1094ed98370fb09a41350ce93a5c9
Ast <varistyshark@gmail.com>
2025-11-11 19:13:22 -0500

    minor changes

--- apps/contracts/src/hooks/LoarHook.sol
+++ apps/contracts/src/hooks/LoarHook.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-//import {ILoarLpLocker} from "../interfaces/ILoarLpLocker.sol";
-//import {ILoarMevModule} from "../interfaces/ILoarMevModule.sol";
 import {ILoarHook} from "../interfaces/ILoarHook.sol";
 import {GovernanceERC20} from "../GovernanceERC20.sol";
 import {IUniverseManager} from "../interfaces/IUniverseManager.sol";
