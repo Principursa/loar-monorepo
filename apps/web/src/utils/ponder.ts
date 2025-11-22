dd66683b738c24729e58050073094468d796641b
Pattisier <Principursa@protonmail.com>
2025-11-22 09:56:59 -0300

    changed ponder url

--- apps/web/src/utils/ponder.ts
+++ apps/web/src/utils/ponder.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@ponder/client";
 import * as schema from "../../../indexer/ponder.schema";
 
-export const client = createClient("http://localhost:42069/sql", { schema });
+const PONDER_URL = import.meta.env.VITE_PONDER_URL || "http://localhost:42069/sql";
+
+export const client = createClient(PONDER_URL, { schema });
